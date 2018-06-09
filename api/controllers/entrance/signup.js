024812cb4a32aa0e2d9d3c6af525a507ad6a091b
Boris Ts. (cborka) <cborka777@yandex.ru>
2018-06-09 17:14:17 +0700

    Тест 2

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -24,7 +24,7 @@ the account verification message.)`,
       type: 'string',
       isEmail: true,
       description: 'The email address for the new account, e.g. m@example.com.',
-      extendedDescription: 'Must be a valid email address.',
+      extendedDescription: 'Должен быть a valid email address.',
     },
 
     password: {
